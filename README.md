2453df39c636a26260c66a306a7d12ea3584b392
Francisco José Beltrán Hernández <fjbeltranh@gmail.com>
2022-10-09 13:47:14 +0200

    primera versión

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# obs-barra-de-vida
+# Barra de Vida
+
+Consideraciones:
+
+* source_name es el nombre de la funte multimedia que servirá como base para las animaciones. Asegúrate de que tenga todas las propiedades desmarcadas.
+* Todo los archivos del script deben estar en la misma carpeta
+* Las imágenes, GIFs o incluso archivos de vídeos que conforman los estados de la barra están en la dirección marcada por el parámetro path (por defecto donde se encuentre el script)
+* Si se quiere reorganizar en carpetas hay que indica folder y opcionalmente folder_anim_mas, folder_anim_menos y folder_estatico.
+* Cada posición del parámetro estado está compuesto por tres elementos.
+    - anim_mas: Es el archivo con la animación creciendo hasta esa posición (no tiene sentido en la primera posición)
+    - anim_menos: Es el archivo con la animación decreciendo hasta esa posición (no tiene sentido en la última posición)
+    - estatico: Es el archivo con el archivo de la posición estática en esa posición (para cuando vuelva o se mantenga)
+* Para asociarlo al stream deck basta con asignarle combianciones de teclas desde obs a las propiedades Menos (decrementa la barra) y Más (incrementa la barra). Una vez hecho eso se puede utilizar en streamdeck dos botones de tipo Atajo con las teclas indicadas.
+
+
+Consejo para esta versión:
+
+* Lo ideal es definir muchos estados (por ejemplo 10) y si se quiere decrementar/aumentar en más de 1, darle tantas veces como se necesite en streamdeck o crear botones de acciones múltiples para cada incremento/decremento
+* Si quieres escuchar un sonido con cada animación, con esta versión sólo sería posible creando la animación como un archivo de vídeo. Puede estudiarse para futuras versioens el incorporar clips de audio independientes con cada animación gif.
--- script-barra-de-vida.lua
+++ script-barra-de-vida.lua
@@ -2,18 +2,13 @@ obs= obslua
 
 
 
--- Variables
+
+-- ---------------------------------------
+-- FUNCIONES GLOBALES
+-- ---------------------------------------
 
 
 
-local is_pressed=false;
-
-
-
-local hotkey_minus=obs.OBS_INVALID_HOTKEY_ID;
-local hotkey_plus=obs.OBS_INVALID_HOTKEY_ID;
-
-print ("base")
 
 -- Hook: La descripción que se muestra en la ventana del Script
 function script_description()
@@ -21,74 +16,19 @@ function script_description()
 	return  
         [[
         <hr/><h1>Barra de Vida</h1>
-        <p>Esto es la descripción</p>
+        <p>Modificar desde config.json</p>
         ]];
 
 end
 
 
 
--- Hook: Las propiedades definidas en la UI
-function script_properties()
-
-    print("properties")
-    props = obs.obs_properties_create()
-    obs.obs_properties_add_text(props, "source_name", "Nombre de la Fuente", obs.OBS_TEXT_DEFAULT)
-    obs.obs_properties_add_int_slider(props, "nivel", "Nivel", 1, num_estados, 1)
-
-    return props
-
-end
-
-
-  
--- Hook: Al cargar el script
-function script_load(settings)
-    print("load")
-
-    -- Hotkey minus
-    hotkey_minus = obs.obs_hotkey_register_frontend("hotkey_minus", "Menos", on_hotkey_minus)
-    local hotkey_minus_array = obs.obs_data_get_array(settings, "hotkey_minus")    
-    obs.obs_hotkey_load(hotkey_minus, hotkey_minus_array)
-    obs.obs_data_array_release(hotkey_minus_array)
-
-    -- Hotkey plus
-    hotkey_plus = obs.obs_hotkey_register_frontend("hotkey_plus", "Más", on_hotkey_plus)
-    local hotkey_plus_array = obs.obs_data_get_array(settings, "hotkey_plus")
-    obs.obs_hotkey_load(hotkey_plus, hotkey_plus_array)
-    obs.obs_data_array_release(hotkey_plus_array)
-
-end
-
-
-
--- Hook: 
-function script_save(settings)
-
-    print("save")
-    -- Hotkey minus
-    local hotkey_minus_array = obs.obs_hotkey_save(hotkey_minus)
-    obs.obs_data_set_array(settings, "hotkey_minus", hotkey_minus_array)
-    obs.obs_data_array_release(hotkey_minus_array)
-
-    -- Hotkey plus
-    local hotkey_plus_array = obs.obs_hotkey_save(hotkey_plus)
-    obs.obs_data_set_array(settings, "hotkey_plus", hotkey_plus_array)
-    obs.obs_data_array_release(hotkey_plus_array)
-
-end
-
-
-
--- 
+-- Hook: Se llama para establecer la configuración predeterminada (si la hay) asociada con el script
 function script_defaults(settings)
 
-    print("defaults")
-    my_settings=settings;
-
-    -- carga la configuracion
+    -- carga la configuracion en variables
     local config=readJson();
-    source_name_default=config["source_name"];
+    local source_name_default=config["source_name"];
     path=config["path"]; if(path=="") then path=script_path(); end;
     folder = config["folder"]; if(folder~="") then folder=folder.."/"; end;
     folder_anim_mas = config["folder_anim_mas"]; if(folder_anim_mas~="") then folder_anim_mas=folder_anim_mas.."/"; end;
@@ -96,35 +36,99 @@ function script_defaults(settings)
     folder_estatico = config["folder_estatico"]; if(folder_estatico~="") then folder_estatico=folder_estatico.."/"; end;
     estados = config["estados"];
     num_estados=table.getn(estados);
-    nivel_default=num_estados;
+    
+    -- variables globales
+    my_settings=settings;
+    is_pressed=false;
+    hotkey_menos=obs.OBS_INVALID_HOTKEY_ID;
+    hotkey_mas=obs.OBS_INVALID_HOTKEY_ID;
+    local nivel_default=num_estados;
     nivel_anterior=nivel_default
 
+    -- fija algunas fuentes
     obs.obs_data_set_default_string(settings, "source_name", source_name_default)
     obs.obs_data_set_default_int(settings, "nivel", nivel_default)
+
+    
+end
+
+
+  
+-- Hook: Se llama para cargar configuraciones específicas al principio del script (se ejecuta después de default)
+function script_load(settings)    
+
+    -- Hotkey menos
+    hotkey_menos = obs.obs_hotkey_register_frontend("hotkey_menos", "Menos", on_hotkey_menos)
+    local hotkey_menos_array = obs.obs_data_get_array(settings, "hotkey_menos")    
+    obs.obs_hotkey_load(hotkey_menos, hotkey_menos_array)
+    obs.obs_data_array_release(hotkey_menos_array)
+
+    -- Hotkey mas
+    hotkey_mas = obs.obs_hotkey_register_frontend("hotkey_mas", "Más", on_hotkey_mas)
+    local hotkey_mas_array = obs.obs_data_get_array(settings, "hotkey_mas")
+    obs.obs_hotkey_load(hotkey_mas, hotkey_mas_array)
+    obs.obs_data_array_release(hotkey_mas_array)
+
+end
+
+
+-- Hook: Se llama cuando se descarga el script al cerrar obs.
+function script_unload()
+
 end
 
 
 
+-- Hook: Se llama cuando se guarda el script. Necesario para cualquier dato de configuración interna adicional (no de  usuario) que pueda utilizar el script
+function script_save(settings)
 
+    -- Hotkey menos
+    local hotkey_menos_array = obs.obs_hotkey_save(hotkey_menos)
+    obs.obs_data_set_array(settings, "hotkey_menos", hotkey_menos_array)
+    obs.obs_data_array_release(hotkey_menos_array)
+
+    -- Hotkey mas
+    local hotkey_mas_array = obs.obs_hotkey_save(hotkey_mas)
+    obs.obs_data_set_array(settings, "hotkey_mas", hotkey_mas_array)
+    obs.obs_data_array_release(hotkey_mas_array)
+
+end
+
+
+
+-- Hook: Se llama cuando el usuario ha cambiado la configuración de la secuencia de comandos (si la hay)
 function script_update(settings)
-
-    print("update")
+    
     source_name = obs.obs_data_get_string(settings, "source_name")
     nivel = obs.obs_data_get_int(settings, "nivel")
     incremento = obs.obs_data_get_int(settings, "incremento")
     is_pressed=true
-
-    print("nivel="..nivel)
+  
 end
 
 
 
+-- Hook: Las propiedades definidas en la UI
+function script_properties()
+    
+    local props = obs.obs_properties_create()
+    obs.obs_properties_add_text(props, "source_name", "Nombre de la Fuente", obs.OBS_TEXT_DEFAULT)
+    obs.obs_properties_add_int_slider(props, "nivel", "Nivel", 1, num_estados, 1)
 
+    -- obs.obs_properties_add_button(props, "menos_button", "Menos", on_hotkey_menos)
+    -- obs.obs_properties_add_button(props, "mas_button", "Mas", on_hotkey_mas)
+
+    return props
+
+end
+
+
+
+-- Hook: Se llama constantemente por cada frame de procesamiento
 function script_tick(seconds)
 
-
     if is_pressed then
-        print("tick")      
+           
         local source = obs.obs_get_source_by_name(source_name)
         if source then
 
@@ -154,7 +158,7 @@ function script_tick(seconds)
 
     end
 
-    is_pressed=false;
+    is_pressed=false
 
 end
 
@@ -162,12 +166,17 @@ end
 
 
 
+-- ---------------------------------------
+-- FUNCIONES LOCALES
+-- ---------------------------------------
 
-function on_hotkey_minus(pressed)
+
+
+-- acción al pulsar la hotkey "menos"
+function on_hotkey_menos(pressed)
 
     is_pressed = pressed
-    if(pressed) then      
-        print("minus")   
+    if(pressed) then     
         if(nivel>1) then 
             obs.obs_data_set_int(my_settings, "nivel", nivel-1)
             script_update(my_settings)
@@ -179,11 +188,11 @@ function on_hotkey_minus(pressed)
 end
 
 
-function on_hotkey_plus(pressed)
+-- acción al pulsar la hotkey "mas"
+function on_hotkey_mas(pressed)
 
     is_pressed = pressed
     if(pressed) then   
-        print("plus")
         if(nivel<num_estados) then
             obs.obs_data_set_int(my_settings, "nivel", nivel+1)
             script_update(my_settings)
@@ -195,6 +204,7 @@ function on_hotkey_plus(pressed)
 end
 
 
+-- lee el archivo config.json que está en la misma carpeta que el script y lo devuelve como un objeto
 function readJson()
 
     local json = require("dkjson")
@@ -203,5 +213,8 @@ function readJson()
     obs.obs_data_release(json_data)
     return json.decode(content)
 
-  
 end
+
+
+
+
